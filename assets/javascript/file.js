8bdf2e2aeb19e19711b3dbe4bd211c621720f41b
Nick Kidder <nickidder@yahoo.com>
2019-06-30 14:18:43 -0700

    fixed api link and key

--- assets/javascript/file.js
+++ assets/javascript/file.js
@@ -1,4 +1,7 @@
-var xhr = $.get("https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=CQBjbfAtYTJ9rknSsiu5pAmXPIaRYJZa&limit=5");
-xhr.done(function(data) { console.log("success got data", data); });
-
+$.ajax({
+    url: "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=CQBjbfAtYTJ9rknSsiu5pAmXPIaRYJZa&limit=5",
+    method: "GET"
+}).then(function(response){
+    console.log(response);
+})
 // var topics  = ["The Office", "Portlandia", "Parks and Recreation", "Always Sunny In Philadelphia", ""]
