9772c53e2ee031be288f2cbde3366e78e9907597
Nick Kidder <nickidder@yahoo.com>
2019-06-30 18:33:55 -0700

    added value to search box

--- assets/javascript/file.js
+++ assets/javascript/file.js
@@ -1,7 +1,19 @@
-$.ajax({
-    url: "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=CQBjbfAtYTJ9rknSsiu5pAmXPIaRYJZa&limit=5",
-    method: "GET"
-}).then(function(response){
-    console.log(response);
+$('#search').on('click', function (e) {
+    e.preventDefault();
+    var userInput = $('#user-input').val()
+    axios({
+        url: "https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=CQBjbfAtYTJ9rknSsiu5pAmXPIaRYJZa&limit=" + userInput,
+        method: "GET"
+    })
+        .then(function (response) {
+            console.log(response.data);
+        })
+        .catch(function (err) {
+            console.error(err)
+        })
 })
-// var topics  = ["The Office", "Portlandia", "Parks and Recreation", "Always Sunny In Philadelphia", ""]
+//create an array of topics
+var topics = ["The Office", "Portlandia", "Parks and Recreation", "Always Sunny In Philadelphia"]
+//create labeled buttons that will load gifs
+//make it so if the user clicks on a gif it will play and if they click it again it will pause
+//make a way so that the user and search for new gifs and they will load to the page
